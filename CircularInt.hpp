8fe5d549f7b15133521275b31c48f91d2d4df9bc
tomer_zaks <tomer_zaks@hotmail.com>
2018-04-29 07:26:55 +0000

    <= >= funcs

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -126,6 +126,15 @@ class CircularInt{
         friend bool operator<(const int& arg1,const CircularInt& arg2);
         friend bool operator<(const CircularInt& arg1,const int& arg2);
         
+         /**
+         * CLASS <= CLASS
+         * INT <= CLASS
+         * CLASS <= INT
+         */
+        friend bool operator<=(const CircularInt& arg1,const CircularInt& arg2);
+        friend bool operator<=(const int& arg1,const CircularInt& arg2);
+        friend bool operator<=(const CircularInt& arg1,const int& arg2);
+        
         
          /**
          * CLASS < CLASS
@@ -135,6 +144,15 @@ class CircularInt{
         friend bool operator>(const CircularInt& arg1,const CircularInt& arg2);
         friend bool operator>(const int& arg1,const CircularInt& arg2);
         friend bool operator>(const CircularInt& arg1,const int& arg2);
+        
+         /**
+         * CLASS <= CLASS
+         * INT <= CLASS
+         * CLASS <= INT
+         */
+        friend bool operator>=(const CircularInt& arg1,const CircularInt& arg2);
+        friend bool operator>=(const int& arg1,const CircularInt& arg2);
+        friend bool operator>=(const CircularInt& arg1,const int& arg2);
         
         CircularInt  & operator= (const CircularInt & other);
         
